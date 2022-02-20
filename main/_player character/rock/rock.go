7dadf3bb193cb0715e70dd12ab17d4d92af44bb8
ZaneF-NEA <87363485+ZaneF-NEA@users.noreply.github.com>
2022-02-20 20:45:15 +0000

    Iteration 4 Adjustments A

--- main/_player character/rock/rock.go
+++ main/_player character/rock/rock.go
@@ -105,7 +105,7 @@ embedded_components {
   data: "sound: \"/assets/sound/rock.ogg\"\n"
   "looping: 0\n"
   "group: \"master\"\n"
-  "gain: 1.0\n"
+  "gain: 0.1\n"
   "pan: 0.0\n"
   "speed: 1.0\n"
   "loopcount: 0\n"
